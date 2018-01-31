6b92bdd0afea7e41a29be6b1dc366f90f4988f38
charles <charlesemery15@gmail.com>
2018-01-31 14:03:31 +0000

    Passed backstage

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -25,13 +25,16 @@ class GildedRose
   def aged(item)
     item.quality = (item.sell_in <= 0 ? item.quality + 2 : item.quality + 1)
     item.sell_in = item.sell_in - 1
-    if item.quality > 50
-      item.quality = 50
-    end
+    item.quality = 50 if item.quality > 50
   end
 
   def backstage(item)
-
+    item.quality += 1
+    item.quality += 1 if item.sell_in < 11
+    item.quality += 1 if item.sell_in < 6
+    item.quality = 0 if item.sell_in <= 0
+    item.quality = 50 if item.quality > 50
+    item.sell_in -= 1
   end
 
   def lengendary(item)
@@ -44,9 +47,7 @@ class GildedRose
 
   def standard(item)
     item.quality = (item.sell_in <= 0 ? item.quality - 2 : item.quality - 1)
+    item.quality = 0 if item.quality <= 0
     item.sell_in = item.sell_in - 1
-    if item.quality < 0
-      item.quality = 0
-    end
   end
 end
